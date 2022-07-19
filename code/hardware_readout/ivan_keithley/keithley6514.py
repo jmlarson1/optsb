6a42493cf3faef3ee42b4a25e8bc679ab9e8cb07
calemhoffman <calem.hoffman@gmail.com>
2022-07-19 11:56:23 -0500

    Update keithley6514.py

--- code/hardware_readout/ivan_keithley/keithley6514.py
+++ code/hardware_readout/ivan_keithley/keithley6514.py
@@ -8,7 +8,7 @@ inst = serial.Serial(port='/dev/ttyUSB0',
 			bytesize=serial.EIGHTBITS,
 			parity=serial.PARITY_NONE,
 			stopbits=serial.STOPBITS_ONE,
-			timeout=5)
+			timeout=1)
 
 if inst.isOpen():
    inst.close()
@@ -19,16 +19,17 @@ inst.write(':CONFigure:VOLTage\r')
 try:
    while True:
 #      inst.write(':MEASure:VOLTage?\r')
-      inst.write(':MEASure:CHARge?\r')
-#      inst.write(':MEASure:CURRent?\r') 
+ #     inst.write(':MEASure:CHARge?\r')
+      inst.write(':MEASure:CURRent?\r') 
       data = inst.read(50)
 #      print(data)
 
       response = data.split(",")
       #could make some below variables for future
-      dbwrite = "influx -execute \'insert fc,tag=cross value={}\' -database=db".format(float(response[0]))
+      value=float(response[0])*(-1000000000000.)
+      dbwrite = "influx -execute \'insert fc,tag=cross value={}\' -database=db".format(value)
       os.system(dbwrite)
-      print("{:.0f}".format(time.time()), response[0])
+      #print("{:.0f}".format(time.time()), response[0])
 except KeyboardInterrupt:
    inst.close()
    pass
