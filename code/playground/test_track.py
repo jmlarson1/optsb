d0df4c2951332fb186b53ec970fa3861bece1ffb
calemhoffman <calem.hoffman@gmail.com>
2022-07-20 10:09:00 -0500

    No major changes

--- code/playground/test_track.py
+++ code/playground/test_track.py
@@ -28,13 +28,14 @@ quad_vals = []
 run_with_testing = 1
 rs = RunTRACK()
 
-for i in range(10):
+for i in range(1):
     run_dir = rs.set_dir()
     # act = np.zeros(9)
     # qd = [1000,-1000,1000]
     #quad_vals = rs.mod_quad_vals(act,qd) #adjusts based on action
     quad_vals = rs.get_quad_vals() # random values
     quad_vals = [1098.47,-1985.25,1242.06]
+    quad_vals = [944.70844639, -2444.46523034, 1524.3123095]
     print(quad_vals)
     rs.set_track(run_dir,quad_vals)
     rs.run_track(run_dir)
